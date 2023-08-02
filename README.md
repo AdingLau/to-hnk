9b1dd5964bc55389138c19c2f6b0e75eb8d4e853
ldqsss <734200940@qq.com>
2023-08-03 00:43:44 +0800

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# love-letter
+
+forked from https://github.com/Hiyurigi/love-letter
+
+做了修改、添加了bgm
---
+++ index.js
@@ -0,0 +1,30 @@
+function domReady(fn) {
+    // If we're early to the party
+    document.addEventListener("DOMContentLoaded", fn);
+    // If late; I mean on time.
+    if (document.readyState === "interactive" || document.readyState === "complete" ) {
+      fn();
+    }
+}
+var flag = true;
+domReady(() => {
+    document.getElementsByClassName("card").item(0).addEventListener("click", function(){
+        document.getElementsByClassName("modal").item(0).classList.add("is-visible");
+    })
+    document.getElementsByClassName("modal").item(0).addEventListener("click", function(e){
+        if(e.target.classList.contains("is-visible")){
+            e.target.classList.remove("is-visible");
+        }
+    })
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var audio = document.getElementById("audio");
+  
+  function playMusic() {
+    audio.play();
+  }
+  // 用户操作触发播放音频
+  document.addEventListener("click", playMusic);
+});
